6a744bd23d1fb70256cc8793946ccd1fd50ff4a2
John Adeniran <jasolajohn@gmail.com>
2022-11-01 22:02:55 +0100

    Fix:Connected to Localhost Database successful

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 
-app.get('/', (req, res) => {
-    console.log('This is the landing API')
+app.get('/', (req, res, next) => {
+    try{
+        res.send('This is the landing API')
+    }catch(error){
+        next(error)
+    }
+   
 })
 
 
@@ -22,11 +27,11 @@ app.get('/', (req, res) => {
 
  mongoose.connect( process.env.MONGO_URI ,{useNewUrlParser : true})
  .then(()=>{console.log("Connected to MongoDB Successfully")})
- .catch(()=>{console.log("An error occurred while connecting to MongoDB")})
+ .catch((err)=>{
+    console.log(err.message)
+    console.log("An error occurred while connecting to MongoDB")
+})
  
- app.listen(process.env.PORT, () => {
-     console.log('Listening on port, ', process.env.PORT)
- })
 
 
 app.listen(PORT, () => {
